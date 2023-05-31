f2fccf06272c9dd8d4430523944ead54ebeb26ca
Oleksandr <workbov@gmail.com>
2023-05-31 20:53:16 +0200

    End

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import Notiflix from 'notiflix';
-import fetchImages from './fetchImages';
+import axios from 'axios';
 const API_KEY = '36838711-56df4c2f8a3a85471ff3834b0';
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 let searchQuery = '';
 let page = 1;
-let totalHits = 0; // add a new variable here
+let totalHits = 0;
 
 loadMoreBtn.style.display = 'none';
+
+async function fetchImages() {
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+
+  try {
+    const response = await axios.get(url);
+    const images = response.data.hits;
+    totalHits = response.data.totalHits;
+
+    if (images.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      appendImages(images);
+      if (totalHits <= page * 40) {
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        loadMoreBtn.style.display = 'block';
+      }
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('An error occurred while fetching data.');
+  }
+  page += 1;
+}
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   searchQuery = event.currentTarget.elements.searchQuery.value;
 
-  // Check if the search query is empty
   if (!searchQuery.trim()) {
     Notiflix.Notify.failure('Please enter a search query.');
     return;
